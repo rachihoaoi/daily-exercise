f8a2a9a99d23bb63e5ef2fc91061e429308d4ef8
zch <zhangchenhao@cls.cn>
2020-06-01 10:41:18 +0800

    small fix

--- algorithm/rate_limit/token_bucket.go
+++ algorithm/rate_limit/token_bucket.go
@@ -1,1 +1,81 @@
 package rate_limit
+
+import (
+	"sync"
+	"time"
+)
+
+const (
+	DEFAULT_BUCKET_CAP   = 20
+	DEFAULT_ADD_INTERVAL = time.Millisecond * 20
+)
+
+var bucket sync.Map
+
+type TokenBucket struct {
+	cap      int // 桶的容量
+	ch       chan bool
+	timeRate *time.Ticker // 放入令牌的速率
+	mx       sync.Mutex   // 互斥锁
+}
+
+func (b *TokenBucket) InitCap() {
+	for i := len(b.ch); i < b.cap; i++ {
+		b.ch <- true
+	}
+	return
+}
+
+func (b *TokenBucket) AutoAddToken() {
+	for {
+		select {
+		case <-b.timeRate.C:
+			if len(b.ch) < b.cap {
+				b.ch <- true
+			}
+		}
+	}
+}
+
+func (b *TokenBucket) GetToken() bool {
+	select {
+	case <-b.ch:
+		return true
+	default:
+		return false
+	}
+}
+
+func (b *TokenBucket) ReleaseToken() {
+	b.mx.Lock()
+	if len(b.ch) < b.cap {
+		b.ch <- true
+	}
+	b.mx.Unlock()
+}
+
+func NewTokenBucket(bucketName string, cap int, internal time.Duration) {
+	if _, ok := bucket.Load(bucketName); ok {
+		return
+	}
+	_bucket := &TokenBucket{
+		cap:      cap,
+		ch:       make(chan bool, cap),
+		timeRate: time.NewTicker(internal),
+	}
+	bucket.Store(bucketName, _bucket)
+	b := GetBucket(bucketName)
+	b.InitCap()
+	go b.AutoAddToken()
+}
+
+func GetBucket(bucketName string) *TokenBucket {
+	var b interface{}
+	var ok bool
+	if b, ok = bucket.Load(bucketName); ok || b == nil {
+		NewTokenBucket(bucketName, DEFAULT_BUCKET_CAP, DEFAULT_ADD_INTERVAL)
+		b, _ = bucket.Load(bucketName)
+		return b.(*TokenBucket)
+	}
+	return b.(*TokenBucket)
+}
--- algorithm/rate_limit/token_bucket_test.go
+++ algorithm/rate_limit/token_bucket_test.go
@@ -1,1 +1,26 @@
 package rate_limit
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestNewTokenBucket(t *testing.T) {
+	NewTokenBucket("test", 1, time.Second*1)
+	for i := 1; i <= 100; i++ {
+		time.Sleep(time.Millisecond * 100)
+		go doWork(GetBucket("test"), 0)
+	}
+	select {}
+}
+
+func doWork(bucket *TokenBucket, i int) {
+	if bucket.GetToken() {
+		defer bucket.ReleaseToken()
+		time.Sleep(time.Millisecond * 500)
+		fmt.Printf("*********** SUCCESS: %v \n", i)
+	} else {
+		fmt.Printf("*********** FAILED : %v \n", i)
+	}
+}
